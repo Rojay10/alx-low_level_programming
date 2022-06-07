584c5f067f8ed7f975a341fb91ae3bf19c80b33e
Rojay10 <rojay0001@gmail.com>
2022-06-06 20:41:15 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
+
 #include <string.h>
+
+
 
 /**
  *
- *  * main - prints string*
+ *  * main - prints string
+ *
  *   * with a new line
+ *
  *    * Return: 0
- */
+ *
+ *     */
+
 int main(void)
+
 {
-		/* print a string to stdout */
+
+		/* print a string to stdout
+		 * 
+		 * 	*/
+
 		putchar('"');
+
 			puts("Programming is like building a multilingual puzzle");
+
 				return (0);
+
 }
