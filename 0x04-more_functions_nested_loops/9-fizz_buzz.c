077bae9c3fbc51589aedd5b6ed60fd472f594dfd
Rojay10 <rojay0001@gmail.com>
2022-06-09 18:39:47 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,16 +15,15 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf(" Fizz");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf(" Buzz");
+			printf("Buzz");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" FizzBuzz");
-
+			printf("FizzBuzz");
 		}
 		else if (i == 1)
 		{
@@ -32,7 +31,7 @@ int main(void)
 		}
 		else
 		{
-			printf(" %d", 1);
+			printf("%d", 1);
 		}
 	}
 	printf("\n");
