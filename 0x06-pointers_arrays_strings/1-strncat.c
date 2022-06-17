abacd536fa7fa132cf8b2e32311ca1714bb70af5
Rojay10 <rojay0001@gmail.com>
2022-06-16 17:27:02 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,6 +9,17 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(*dest, *src, n);
+	int i = 0, j = 0;
+
+	while (*(dest + i))
+	{
+		i++;
+	}
+	while (*(src + j) != '\0' && j < n)
+	{
+		*(dest + i) = src[j];
+		i++;
+		j++;
+	}
 	return (dest);
 }
